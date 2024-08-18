676168857dec70d1d50e3225f63280b4023416d6
Park Joo won <qkrwndnjs7308@gmail.com>
2024-08-18 23:31:55 +0900

    [level 2] Title: 하노이의 탑, Time: 4.21 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,36 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    private static List<int[]> ansList;
+    private static List<int[]> arr = new ArrayList<>();
 
-    public static int[][] solution(int n) {
-
-        ansList = new ArrayList<>();
+    public int[][] solution(int n) {
         
-        dfs(n, 1,3, 2);
-
-
-        int[][] answer = new int [ansList.size()] [];
-        for(int i=0; i<ansList.size(); i++){
-            answer[i] = ansList.get(i);
+        move(n, 1, 2, 3);
+        
+        int[][] answer = new int[arr.size()][];
+        for(int i = 0; i< arr.size(); i++){
+            answer[i] = arr.get(i);
         }
-
+        
         return answer;
     }
 
-    private static void dfs(int n, int start, int to, int mid) {
-        if(n == 1){
-            ansList.add(new int[]{start, to});
+    private static void move(int n, int start, int mid, int end) {
+        if (n == 0) {
             return;
         }
-        // 1번 기둥의 n-1개를 3번을 걸쳐 2번으로 이동시킴
-        dfs(n-1, start, mid, to);
-
-        // 가장 큰 n을 1에서 3으로 이동시킴
-        ansList.add(new int[]{start, to});
-
-        // 2번의 기둥의 n-1개를 1번을 걸쳐 3번으로 이동시킴
-        dfs(n-1, mid, to, start);
+        
+        move(n-1,start,end,mid);
+        
+        arr.add(new int[]{start,end});
+        
+        move(n-1,mid,start,end);
+        
     }
 }
