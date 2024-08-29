fca8d23dc17da08c69a341b3d7a398b7525eee03
Park Joo won <qkrwndnjs7308@gmail.com>
2024-08-29 15:17:24 +0900

    [Bronze V] Title: A+B, Time: 168 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+	
+	Scanner in = new Scanner(System.in);
+	
+    int a = in.nextInt();
+    int b= in.nextInt();
+    
+    int num = a+b;
+    
+    System.out.print(num);
+		
+	}
+}
