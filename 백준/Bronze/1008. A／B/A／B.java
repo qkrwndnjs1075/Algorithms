23209dc9a7c0001f70eb55fd69cecfd0359cd250
Park Joo won <qkrwndnjs7308@gmail.com>
2024-08-31 00:10:50 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 17952 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+	
+	Scanner in = new Scanner(System.in);
+	
+    double a = in.nextDouble();
+    double b= in.nextDouble();
+    
+    double num = a/b;
+    
+    System.out.print(num);
+		
+	}
+}
