b10a1d57c14588be5e5de02d22e73d9ac0b72111
Park Joo won <qkrwndnjs7308@gmail.com>
2024-09-03 17:22:28 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 192 ms, Memory: 18252 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M_sum = 0;
+        int Y_sum = 0;
+        int sec = 0;
+
+        
+        for (int i = 0; i < N; i++) {
+            sec = sc.nextInt();
+            Y_sum += ((sec/30) +1) * 10;
+            M_sum += ((sec/60)+1) * 15;
+        }
+		
+		  if (M_sum == Y_sum) {
+            System.out.println("Y M " + Y_sum);
+        } else if (M_sum < Y_sum) {
+            System.out.println("M " + M_sum);
+        } else if (M_sum > Y_sum) {
+            System.out.println("Y " + Y_sum);
+        }
+		
+	}
+	
+}
