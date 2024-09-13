64064301957f394dd55180e1715780e785654248
Park Joo won <qkrwndnjs7308@gmail.com>
2024-09-13 12:13:27 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,3 +1,17 @@
 function solution(my_string, is_suffix) {
   return my_string.endsWith(is_suffix) ? 1 : 0;
 }
+
+function solution(my_string, is_suffix) {
+    var answer = 0;
+    for(let i=0;i<my_string.length;i++){
+        if(my_string.slice(i) == is_suffix){ // i번째 문자부터 끝까지의 부분 문자열이 is_suffix와 동일한지 검사
+            return 1;
+        }
+    }
+    return answer;
+}
+
+
+
+
