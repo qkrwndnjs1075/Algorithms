4714e8e1fc4bce4813035f367fc91efa4b09de96
Park Joo won <qkrwndnjs7308@gmail.com>
2024-10-08 15:52:04 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+    var answer = 0;
+    var answer2 = 0;
+    
+    for(let i=0; i<num_list.length; i++){
+        if(i%2==0){
+            answer += num_list[i];
+        }else
+            answer2 += num_list[i];
+    }
+    if(answer<answer2){
+        return answer2
+    }else return answer
+    
+    
+}
