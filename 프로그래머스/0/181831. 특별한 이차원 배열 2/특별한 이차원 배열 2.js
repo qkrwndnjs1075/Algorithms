9deac13cc92e9004acf7bdd96e23ec0b5bc400ac
Park Joo won <qkrwndnjs7308@gmail.com>
2024-11-05 15:46:46 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.61 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,17 +1,3 @@
 function solution(arr) {
-    var answer = 0;
-    for(let i=0;i<arr.length;i++){
-        for(let j=0;j<arr.length;j++){
-            if(arr[i][j]!==arr[j][i]) {
-
-               return answer=0
-            }
-            else {answer=1}
-        }
-    }
-    return answer;
+   return arr.every((row, i) => row.every((val, j) => val === arr[j][i])) ? 1 : 0;
 }
-
-console.log(solution([[5, 192, 33], [192, 72, 95], [33, 95, 999]]))
-console.log(solution([[19, 498, 258, 587], [63, 93, 7, 754], [258, 7, 1000, 723],
- [587, 754, 723, 81]]))
