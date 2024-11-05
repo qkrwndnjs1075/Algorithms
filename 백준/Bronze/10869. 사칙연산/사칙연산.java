633848825bfecc1317c235bcda2c4143d2fcfdb1
Park Joo won <qkrwndnjs7308@gmail.com>
2024-11-05 10:23:57 +0900

    [Bronze V] Title: 사칙연산, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int s1 = sc.nextInt(), s2 = sc.nextInt();
+        int sum =0;
+        
+        System.out.println(s1 + s2);
+        System.out.println(s1 - s2);
+        System.out.println(s1 * s2);
+        System.out.println(s1 / s2);
+        System.out.println(s1 % s2);
+        
+        
+    }
+}
