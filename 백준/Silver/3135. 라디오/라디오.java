3139b5eb7b2b635185b5ac954d948c4a48c385f7
주원 <qkrwndnjs7308@gmail.com>
2025-03-06 10:47:09 +0900

    [Silver V] Title: 라디오, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/3135. 라디오/라디오.java
+++ 백준/Silver/3135. 라디오/라디오.java
@@ -1,25 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-
-        int min = Math.abs(A - B);
-        int N = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            int tmp = Integer.parseInt(br.readLine());
-            min = Math.min(Math.abs(B - tmp) + 1, min);
-        }
-
-        System.out.println(min);
-        br.close();
-    }
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+	    int A = Integer.parseInt(st.nextToken());
+	    int B = Integer.parseInt(st.nextToken());
+	    
+	    int min = Math.abs(A-B);
+	    
+	    int N = Integer.parseInt(br.readLine());
+	    
+	    for(int i=0; i<N; i++){
+	        int tmp = Integer.parseInt(br.readLine());
+	        
+	        min = Math.min(Math.abs(B-tmp)+1, min);
+	    }
+		System.out.println(min);
+	}
 }
