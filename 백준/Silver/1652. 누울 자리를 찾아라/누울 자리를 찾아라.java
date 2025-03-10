93979f724be70f9778f72aa3cf580818ec986539
주원 <qkrwndnjs7308@gmail.com>
2025-03-10 09:23:16 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 128 ms, Memory: 15996 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.java
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.java
@@ -1,42 +1,42 @@
 import java.util.*;
+import java.io.*;
 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+public class Main
+{
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
 		
 		char[][] map = new char[101][101];
-		int horizontal = 0; // 가로 개수
-		int vertical = 0; // 세로 개수
+		int horizontal = 0; // 가로
+		int vertical = 0; // 새로
 		
-		// 입력 받기
-		for(int i=0; i<n; i++) {
-			String s = sc.next();
-			for(int j=0; j<n; j++) {
-				map[i][j] = s.charAt(j);
-			}
+		for(int i=0; i<N; i++){
+		    String s = br.readLine();
+		    for(int j = 0; j<N; j++){
+		        map[i][j] = s.charAt(j);
+		    }
 		}
 		
-		// 가로, 세로 방향으로 누울 수 있는지 여부
-		for(int i=0; i<n; i++) {
-			int check_h = 0, check_v = 0;
-			for(int j=0; j<n; j++) {
-				// 가로 체크
-				if(map[i][j] == '.') check_h++;
-				if(map[i][j] == 'X' || (j == n-1)) {
-					if(check_h >= 2) horizontal++;
-					check_h = 0;
-				}
-				
-				// 세로 체크
-				if(map[j][i] == '.') check_v++;
-				if(map[j][i] == 'X' || (j == n-1)) {
-					if(check_v >= 2) vertical++;
-					check_v = 0;
-				}
-			}
+		for(int i=0; i<N; i++){
+		    int check_h=0, check_v = 0;
+		    for(int j=0; j<N; j++){
+		        if(map[i][j] == '.') check_h++;
+		        if(map[i][j] == 'X' || (j== N-1)){
+		            if(check_h >= 2) horizontal++;
+		            check_h = 0;
+		        }
+		        
+		        if(map[j][i] == '.') check_v++;
+		        if(map[j][i] == 'X' || (j == N-1)){
+		            if(check_v >=2) vertical++;
+		            check_v = 0;
+		        }
+		    }
 		}
+		System.out.println(horizontal +" "+vertical);
 		
-		System.out.println(horizontal + " " + vertical);
 	}
 }
