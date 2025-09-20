80a54060048bc5c738e593f08bae54425a8b36ab
주원 <qkrwndnjs7308@gmail.com>
2025-09-20 17:45:42 +0900

    [level 2] Title: 시소 짝꿍, Time: 67.13 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,55 +1,46 @@
 import java.util.*;
 
 class Solution {
-   public long solution(int[] weights) {
-       Map<Integer, Integer> weightCount = new HashMap<>();
-       
-       // 무게별 개수 카운트
-       for (int weight : weights) {
-           weightCount.put(weight, weightCount.getOrDefault(weight, 0) + 1);
-       }
-       
-       long result = 0;
-       Set<String> processedPairs = new HashSet<>();
-       
-       for (Map.Entry<Integer, Integer> entry1 : weightCount.entrySet()) {
-           int weight1 = entry1.getKey();
-           int count1 = entry1.getValue();
-           
-           for (Map.Entry<Integer, Integer> entry2 : weightCount.entrySet()) {
-               int weight2 = entry2.getKey();
-               int count2 = entry2.getValue();
-               
-               if (weight1 > weight2) continue; // 중복 방지
-               
-               String pairKey = weight1 + "," + weight2;
-               if (processedPairs.contains(pairKey)) continue;
-               processedPairs.add(pairKey);
-               
-               if (weight1 == weight2) {
-                   // 같은 무게인 경우: nC2 = n * (n-1) / 2
-                   result += (long) count1 * (count1 - 1) / 2;
-               } else if (canBalance(weight1, weight2)) {
-                   // 다른 무게지만 균형 가능한 경우
-                   result += (long) count1 * count2;
-               }
-           }
-       }
-       
-       return result;
-   }
-   
-   private boolean canBalance(int weight1, int weight2) {
-       int[] distances = {2, 3, 4};
-       
-       // weight1 * d1 = weight2 * d2 인지 확인
-       for (int d1 : distances) {
-           for (int d2 : distances) {
-               if (weight1 * d1 == weight2 * d2) {
-                   return true;
-               }
-           }
-       }
-       return false;
-   }
+    public long solution(int[] weights) {
+        long answer = 0; // answer 변수가 없어서 추가했습니다.
+        
+        Arrays.sort(weights);
+        
+        HashMap<Integer, Long> weightCounts = new HashMap<>();
+        
+        for(int w : weights) {
+            
+            // 1:1 비율
+            if(weightCounts.containsKey(w)) {
+                answer += weightCounts.get(w);
+            }
+            
+            // 1:2 비율 (2m, 4m)
+            if(w % 2 == 0) {
+                int partner = w / 2;
+                if(weightCounts.containsKey(partner)) {
+                    answer += weightCounts.get(partner);
+                }
+            }
+            
+            // 2:3 비율 (2m, 3m) - 수정된 부분
+            if(w % 3 == 0) {
+                int partner = w * 2 / 3;
+                if(weightCounts.containsKey(partner)) {
+                    answer += weightCounts.get(partner);
+                }
+            }
+            
+            // 3:4 비율 (3m, 4m) - 수정된 부분
+            if(w % 4 == 0) {
+                int partner = w * 3 / 4;
+                if(weightCounts.containsKey(partner)) {
+                    answer += weightCounts.get(partner);
+                }
+            }
+            
+            weightCounts.put(w, weightCounts.getOrDefault(w, 0L) + 1);
+        }
+        return answer;
+    }
 }
