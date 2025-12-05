2b43cf5029a83e050defc74c89c95c865fcf4e5d
박주원 <qkrwndnjs7308@gmail.com>
2025-12-05 16:43:54 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.08 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,46 +1,47 @@
 class Solution {
+    
     public int solution(int[] bandage, int health, int[][] attacks) {
+        
         int bandageTime = bandage[0];
         int recoveryPerSec = bandage[1];
         int bonusRecovery = bandage[2];
-
+        
         int currentHealth = health;
         int maxHealth = health;
         int comboCount = 0;
         int attackIndex = 0;
-
+        
         int lastAttackTime = attacks[attacks.length - 1][0];
-
-        for (int time = 1; time <= lastAttackTime; time++) {
-            // 몬스터 공격 여부 확인
-            if (attackIndex < attacks.length && time == attacks[attackIndex][0]) {
+        
+        for(int i = 1; i <= lastAttackTime; i++){
+            
+            if(attackIndex < attacks.length && i == attacks[attackIndex][0]){
+                
                 int damage = attacks[attackIndex][1];
                 currentHealth -= damage;
                 comboCount = 0;
+                
                 attackIndex++;
-
-                // 캐릭터 사망 여부 확인
-                if (currentHealth <= 0) {
+                
+                if(currentHealth <= 0) {
                     return -1;
                 }
+                
             } else {
-                // 체력 회복
                 currentHealth += recoveryPerSec;
                 comboCount++;
-
-                // 연속 성공 보너스
-                if (comboCount == bandageTime) {
+                
+                if(comboCount == bandageTime) {
                     currentHealth += bonusRecovery;
                     comboCount = 0;
                 }
-
-                // 최대 체력 제한
-                if (currentHealth > maxHealth) {
+                
+                if(currentHealth > maxHealth) {
                     currentHealth = maxHealth;
                 }
             }
         }
-
+        
         return currentHealth;
-    }
+    } 
 }
