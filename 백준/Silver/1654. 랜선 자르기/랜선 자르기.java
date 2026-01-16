78bb63d29a7611bb5519b6dd429164caa0956322
박주원 <qkrwndnjs7308@gmail.com>
2026-01-16 14:08:47 +0900

    [Silver II] Title: 랜선 자르기, Time: 148 ms, Memory: 17288 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,66 +1,53 @@
 import java.util.*;
+import java.io.*;
 
 public class Main
 {
-	public static void main(String[] args) {
+    static int N = 0;
+    static int K = 0;
+    static int[] arr;
+ 
+	public static void main(String[] args) throws Exception {
 		
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+	    K = Integer.parseInt(st.nextToken());
+	    N = Integer.parseInt(st.nextToken());
 		
-		int K = sc.nextInt();
+		arr = new int[K];
+		long left = 1;
+		long right = 0;
 		
-		int N = sc.nextInt();
-		
-		int[] arr = new int[K];
-		
-		long max = 0;
-		
-		for(int i=0; i<K; i++){
-		    arr[i] = sc.nextInt();
-		    
-		    if(max < arr[i]){
-		        max = arr[i];
-		    }
-		}
-		
-		max++;
-		
-		long min = 0;
-		long mid = 0;
-		
-		while(min<max){
-		    
-		    
-		    
-		    mid = (max + min) / 2;
-		    
-		    long count = 0;
-		    
-		    for(int i=0; i<arr.length; i++){
-		        
-		        count += (arr[i]/mid);
-		    }
-		    
-		    if(count<N){
-		        
-		        max = mid;
-		        
-		    }else{
-		        
-		        min = mid + 1;
-		        
-		    }
-		    
-		    
-		    
-		    
-		}
-		
-		System.out.println(min - 1);
-		
-		
-		
-		
+        for(int i = 0; i<K; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            right = Math.max(right, arr[i]);
+        }
+        
+        long answer = 0;
+        
+        while(left <= right) {
+            long mid = left + (right - left) / 2;
+            
+            if(canMake(mid)) {
+                left = mid + 1;
+                answer = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        System.out.println(answer);
+        
+	}
+	
+	static boolean canMake(long mid) {
+	    long count = 0;
+	    for(int x : arr) {
+	        count += x / mid;
+	    }
+	    
+	    return count >= N;
 	}
 }
